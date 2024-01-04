772c29345e16fd7d89fb40bcd8e615b633897cd6
Olga <olga.salagina1988@yandex.ru>
2024-01-04 20:57:58 +0300

    Main.java added

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+    public int calculate(int weightKg, double heightM) {
+        double bmi = weightKg / (heightM * heightM);
+        return (int) bmi;
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BmiService service = new BmiService();
+        int weightKg = 65;
+        double heightM = 2;
+        String bodyMassIndex;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        bodyMassIndex = String.valueOf(String.valueOf(service.calculate(weightKg, heightM)));
+        System.out.println(bodyMassIndex);
     }
 }
